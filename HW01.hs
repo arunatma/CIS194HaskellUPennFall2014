9aa8ffd6ee3cfd9a264dc0e43d46bf3a58ca55c8
Arunram A <arunram.atma@gmail.com>
2014-09-12 23:41:39 +0530

    Initial Commit
    Solved problems pertaining to the Week 1.

---
+++ HW01.hs
@@ -0,0 +1,47 @@
+{-# OPTIONS_GHC -Wall #-}
+import Data.List
+
+-- Solutions for http://www.seas.upenn.edu/~cis194/hw/01-intro.pdf
+-- https://www.dropbox.com/s/rkccfj41hwpqhps/01-intro.pdf?dl=0
+
+lastDigit :: Integer -> Integer
+lastDigit x = x `mod` 10 
+
+dropLastDigit :: Integer -> Integer
+dropLastDigit x = x `div` 10
+
+toDigits :: Integer -> [Integer]
+toDigits x
+    | x <= 0    = []
+    | otherwise = intercalate [] [toDigits (dropLastDigit x), [lastDigit x]]
+
+-- takes in two lists (of any type) and interleaves them and gives a single list
+interleave :: [a] -> [a] -> [a]
+interleave _ [] = []
+interleave [] _ = []
+interleave (x:xs) (y:ys) = x : y : (interleave xs ys)
+    
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther xs = reverse $ 
+    zipWith (*) (reverse xs) 
+                (interleave (repeat 1) (repeat 2))
+
+sumDigits :: [Integer] -> Integer
+sumDigits xs = sum $ intercalate [] (map toDigits xs)
+
+validate :: Integer -> Bool
+validate x 
+    | reminder == 0 = True
+    | otherwise = False
+    where reminder = (sumDigits $ doubleEveryOther $ toDigits x) `mod` 10
+    
+
+type Peg = String
+type Move = (Peg, Peg)
+
+hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
+hanoi 0 _ _ _ = []
+hanoi 1 a b _ = [(a, b)]
+hanoi n a b c = 
+    intercalate [] [hanoi (n - 1) a c b, hanoi 1 a b c, hanoi (n - 1) c b a]
+    
---
+++ README.md
@@ -0,0 +1,14 @@
+An attempt to solve 
+
+# Home Work Problems of CIS 194 Introduction to Haskell
+
+    **Fall 2014**
+
+# Class Site:
+    http://www.seas.upenn.edu/~cis194/
+    
+HW Questions:
+1. Week 1: https://www.dropbox.com/s/rkccfj41hwpqhps/01-intro.pdf?dl=0
+2. Week 2: https://www.dropbox.com/s/4ytftmey6nmcd18/02-dict.pdf?dl=0
+
+
